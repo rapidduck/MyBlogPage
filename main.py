f19bfcce284ee5e885e548a62511a65d7fd20f95
rapidduck <robin.skaba@volny.cz>
2022-02-11 19:42:01 +0100

    removed create_all

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, ForeignKey("blog_posts.id"))
     post = relationship("BlogPost", back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 def admin_only(function):
     def inner_func():
